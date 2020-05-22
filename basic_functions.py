d67d3a0ecdb767953657ed4399eaf9416bd206b4
tellgreenpo <60693210+tellgreenpo@users.noreply.github.com>
2020-05-22 15:37:31 +0200

    Changed the keys and fixed some bugs

--- basic_functions.py
+++ basic_functions.py
@@ -1,32 +1,13 @@
 import random
 import ia_functions
 
-player1 = {
-    "sticks_left": 10
-}
+#player1 = {
+#    "sticks_left": 10
+#}
 
-player2 = {
-    "sticks_left": 10
-}
-
-# Returns if a game is finished and print who is the winner between 2 players
-def winner_pvp(player1, player2):
-    if player1["sticks_left"] <= 0:
-        winner = 2
-    elif player2["sticks_left"] <= 0:
-        winner = 1
-    else:
-        winner = 0
-    return winner
-
-def winnerVsAi(ai,player):
-    if ai.sticks <= 0:
-        winner = 2
-    elif player["sticks_left"] <= 0:
-        winner = 1
-    else:
-        winner = 0
-    return winner
+#player2 = {
+#    "sticks_left": 10
+#}
 
 
 # The game itself between 2 human players
@@ -38,7 +19,7 @@ def game(player1, player2):
         turn1 = int(input())
         sticks = sticks - turn1
         player1["sticks_left"] = sticks
-        if winner_pvp(player1, player2) != 0:
+        if sticks <= 0:
             print("Player2 wins")
             break
         else:
@@ -47,32 +28,33 @@ def game(player1, player2):
             turn2 = int(input())
             sticks = sticks - turn2
             player2["sticks_left"] = sticks
-        if winner_pvp(player1, player2) != 0:
+        if sticks <= 0:
             print("Player1 wins")
             break
 
 def iaGame(player):
     sticks = 10
     ai = ia_functions.Ai(10)
-    ai.createdatabase()
-    ai.memory = ai.extract_memory()
+    ai.memory = ai.extract_memory("training_result.txt")
     while True:
         print("There are " + str(sticks) + " left in the game")
         print("Player1 how manny sticks do you want to take : 1, 2, 3 ?")
         turn1 = int(input())
+        ai.playerposition.append(sticks)
         sticks = sticks - turn1
         player["sticks_left"] = sticks
-        if winnerVsAi(ai, player) != 0:
+        if sticks <= 0:
             print("ai wins")
             break
         else:
-            ai.the_chosen_one = ai.choice(ai.memory,sticks)
-            print("The ai is taking "+str(ai.the_chosen_one)+" sticks")
-            sticks -= ai.the_chosen_one
+            the_chosen_one = ai.choice(ai.memory,sticks)
+            print("The ai is taking "+str(the_chosen_one)+" sticks")
+            sticks -= the_chosen_one
             ai.sticks = sticks
             player["sticks_left"] = sticks
-            if winnerVsAi(ai,player) != 0:
-                ai.modify_data(sticks+ai.the_chosen_one, ai.the_chosen_one)
+            if sticks <= 0:
+                print("test")
+                ai.modify_data(the_chosen_one, sticks+the_chosen_one, ai.playerposition)
                 print("player wins")
                 print(ai.memory)
                 break
@@ -80,20 +62,28 @@ def iaGame(player):
 def training(length):
     ai = ia_functions.Ai(10)
     ai.createdatabase()
-    ai.memory = ai.extract_memory()
+    ai.memory = ai.extract_memory("memoryai.txt")
     for times in range(length):
         sticks = 10
         while True:
+            ai.playerposition.append(sticks)
             training_choice = random.choice([1,2,3])
             sticks -= training_choice
             if sticks <= 0:
+                ai.playerposition.clear()
                 break
-            ai.the_chosen_one = ai.choice(ai.memory, sticks)
-            sticks -= ai.the_chosen_one
+            the_chosen_one = ai.choice(ai.memory, sticks)
+            sticks -= the_chosen_one
             ai.sticks = sticks
             if sticks <= 0:
-                ai.modify_data(sticks + ai.the_chosen_one, ai.the_chosen_one)
+                ai.modify_data(the_chosen_one,sticks+the_chosen_one, ai.playerposition)
+                ai.playerposition.clear()
                 break
     f = open("training_result.txt", "w")
     f.write(str(ai.memory))
     f.close()
+
+
+player = {'sticks_left' : 10}
+training(5000000)
+iaGame(player)
--- ia_functions.py
+++ ia_functions.py
@@ -1,48 +1,33 @@
 import random
 
 
-# definition object IA
-
-
 class Ai:
-    # Create AI database, each key represent the number of sticks left in the game
-    # AI_memory = {
-    # "1": {"sticks_left": 1, "A": 1, "B": 1, "C": 1},
-    # "2": {"sticks_left": 2, "A": 1, "B": 1, "C": 1},
-    # "3": {"sticks_left": 3, "A": 1, "B": 1, "C": 1},
-    # "4": {"sticks_left": 4, "A": 1, "B": 1, "C": 1},
-    # "5": {"sticks_left": 5, "A": 1, "B": 1, "C": 1},
-    # "6": {"sticks_left": 6, "A": 1, "B": 1, "C": 1},
-    # "7": {"sticks_left": 7, "A": 1, "B": 1, "C": 1},
-    # "8": {"sticks_left": 8, "A": 1, "B": 1, "C": 1},
-    # "9": {"sticks_left": 9, "A": 1, "B": 1, "C": 1},
-    # "10": {"sticks_left": 10, "A": 1, "B": 1, "C": 1},
-    # }
 
     def createdatabase(self):
         f = open("memoryai.txt", "w")
         f.write(str(
             {
-                1: {"A": 1, "B": 1, "C": 1},
-                2: {"A": 1, "B": 1, "C": 1},
-                3: {"A": 1, "B": 1, "C": 1},
-                4: {"A": 1, "B": 1, "C": 1},
-                5: {"A": 1, "B": 1, "C": 1},
-                6: {"A": 1, "B": 1, "C": 1},
-                7: {"A": 1, "B": 1, "C": 1},
-                8: {"A": 1, "B": 1, "C": 1},
-                9: {"A": 1, "B": 1, "C": 1},
-                10: {"A": 1, "B": 1, "C": 1},
+                1: {1: 1, 2: 1, 3: 1},
+                2: {1: 1, 2: 1, 3: 1},
+                3: {1: 1, 2: 1, 3: 1},
+                4: {1: 1, 2: 1, 3: 1},
+                5: {1: 1, 2: 1, 3: 1},
+                6: {1: 1, 2: 1, 3: 1},
+                7: {1: 1, 2: 1, 3: 1},
+                8: {1: 1, 2: 1, 3: 1},
+                9: {1: 1, 2: 1, 3: 1},
+                10: {1: 1, 2: 1, 3: 1},
             }
         ))
         f.close()
 
     def __init__(self, sticks):
         self.sticks = 10
+        self.playerposition = []
         self.memory = {}
 
-    def extract_memory(self): # peut etre modifier le code pour un parametre de l'object etant la memoire
-        f = open("memoryai.txt", "r")
+    def extract_memory(self,filename): # peut etre modifier le code pour un parametre de l'object etant la memoire
+        f = open(filename, "r")
         # rajout de eval pour etre sur de recuperer ola bonne structure de donnees
         file = eval(f.read())
         f.close()
@@ -51,41 +36,34 @@ class Ai:
     # fp = open(filepath, 'r').read()
 
 
-    # return letters equivalent of number of sticks taken
-    def letterchoice(self, choice):
-        if choice == 1:
-            return "A"
-        elif choice == 2:
-            return "B"
-        elif choice == 3:
-            return "C"
-
     # changes the choice to 0 if it is a loss
-    def modify_data(self, sticks, choice):
-        self.memory[sticks][self.letterchoice(choice)] = 0
+    def modify_data(self,choice,sticks, playerposition):
+        # Si on perd la partie, on va alors eviter de refaire les memes decisions
+        number = 1
+        self.memory[sticks][choice] = 0
+        # Pour chaque position du joueur adverse annule le chemin a celle ci car il suceptible de gagner
+        for num in playerposition:
+            while number <= 3 and num+number <= 10:
+                self.memory[num+number][number] = 0
+                number += 1
+
 
     # return the choice of the AI
     def choice(self, memory, sticks):
-        # if every choice is a loss, nullifies the path to it
         current = memory[sticks]
-        if current["A"] == 0 and current["B"] == 0 and current["C"] == 0:
-            number = 1  # number of sticks variable
-            while sticks - number >= 0 and number <= 3:  # limit of sticks is 3
-                self.modify_data(sticks + number, self.letterchoice(number))  # sets the corresponding choice to null
-                number = number + 1
-            # chooses a random number for move according to "weights"
+        if current[1] == 0 and current[2] == 0 and current[3] == 0:
             move = random.randint(1, 3)
-        elif current["A"] == 0 and current["B"] == 0:
+        elif current[1] == 0 and current[2] == 0:
             move = 3
-        elif current["A"] == 0 and current["C"] == 0:
+        elif current[1] == 0 and current[3] == 0:
             move = 2
-        elif current["B"] == 0 and current["C"] == 0:
+        elif current[2] == 0 and current[3] == 0:
             move = 1
-        elif current["A"] == 0:
+        elif current[1] == 0:
             move = random.choice([2, 3])
-        elif current["B"] == 0:
+        elif current[2] == 0:
             move = random.choice([1, 3])
-        elif current["C"] == 0:
+        elif current[3] == 0:
             move = random.choice([1, 2])
         else:
             move = random.randint(1, 3)
